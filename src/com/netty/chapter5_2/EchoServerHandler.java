e5e33fb07c8026468e10dd40f8e22248ad534388
tswstarplanet <tswstarplanet@163.com>
2016-10-07 20:09:30 +0800

    nio，aio

---
+++ src/com/netty/chapter5_2/EchoServerHandler.java
@@ -0,0 +1,29 @@
+package com.netty.chapter5_2;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * Created by weitaosheng on 2016/10/7.
+ */
+public class EchoServerHandler extends ChannelHandlerAdapter {
+
+    int counter = 0;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("This is " + ++counter + " times receive client : [" + msg + "]");
+        String body = (String)msg;
+        ByteBuf echo = Unpooled.copiedBuffer(body.getBytes());
+        ctx.writeAndFlush(echo);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
